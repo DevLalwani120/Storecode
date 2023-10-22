deae8b678efd91763bc28c7a46d10895c98cf4ba
Your Name <you@example.com>
2023-10-22 13:50:54 +0530

    first comit

---
+++ storecode.js
@@ -0,0 +1,18 @@
+  $('.slider-nav').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    focusOnSelect: true,
+    autoplay:true,
+    autoplaySpeed: 3000, // Specify the time interval in milliseconds between slides
+  });
+  $('.slider-nav1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    focusOnSelect: true,
+    autoplay:true,
+    autoplaySpeed: 3000, // Specify the time interval in milliseconds between slides
+  });
